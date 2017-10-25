1da0bf6d8e7b3b8f1a9451ae16a38e3315443a80
huyubo <bailang322@163.com>
2017-10-25 15:05:38 +0800

    测试runtime

---
+++ objc-runtime-master/debug-objc/main.m
@@ -0,0 +1,29 @@
+
+//  main.m
+//  debug-objc
+//
+//  Created by closure on 2/24/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import "objc-runtime.h"
+int main(int argc, const char * argv[])
+{
+    
+    NSObject *obj = [[NSObject alloc] init];
+//    NSLog(@"===:%@",[obj debugDescription]);
+//    NSLog(@"==:%d",[obj isKindOfClass:[NSObject class]]);
+//        const char *className= object_getClassName([obj class]);
+//    printf("%s",className);
+//    NSLog(@"className=:%s",className);
+//    NSLog(@"classd=:%d",className);//1122215---16781599
+//    @autoreleasepool
+//    {
+        //insert code here...
+//        NSLog(@"Hello, World!");
+//        NSMutableArray *array = [[NSMutableArray alloc] init];
+//        NSArray *weakArray = [[NSArray alloc] init];
+//    }
+    return 0;
+}
